fa0669e523604bbbf9efdce3ffcb4058b30275fc
kajal <kajal.r@perfectiongeeks.com>
2022-12-26 11:15:07 +0530

    updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4200;
+const port = 8000;
 const path = require("path");
 // const cors = require('cors')P
 // app.use(cors());
