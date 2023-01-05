0d25cf4f86c785d716914faff957c93b266a405f
Shrey Bhardwaj <shreybhardwaj@192.168.1.199>
2023-01-05 16:24:43 +0530

    updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 3000;
 const path = require("path");
 // const cors = require('cors')P
 // app.use(cors());
@@ -11,12 +11,12 @@ app.listen(port);
 // /home/perfectiongeeks/giveasparkClient/Giveaspark_client/dist/giveaspark-client/index.html - path
 
 // /home/shrey/giveaspark_client/dist/giveaspark-client/index.html- wrong path
-app.use(express.static("dist/giveaspark_client"));
+app.use(express.static("dist/giveaspark-client"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(__dirname + "/dist/giveaspark_client/index.html");
+  res.sendFile(__dirname + "/dist/giveaspark-client/index.html");
 });
 
-app.use(express.static(path.join(__dirname, "dist/giveaspark_client")));
+app.use(express.static(path.join(__dirname, "dist/giveaspark-client")));
 //test commit
 
