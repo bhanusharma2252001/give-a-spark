b211bab404b135a49fbbb3692dcc76638c008cb9
kajalperfectiongeeks <kajal.r@perfectiongeeks.com>
2023-01-17 14:03:35 +0530

    updared

--- src/app/home-dashboard/basic-template/basic-template.component.ts
+++ src/app/home-dashboard/basic-template/basic-template.component.ts
@@ -21,14 +21,14 @@ export class BasicTemplateComponent implements OnInit {
   signatureDetailsForm: FormGroup
   planShow = false;
   public toggle: boolean = false;
-showPro:boolean=false;
-QuoteId:any;
-LongQuote:any;
-  ShowOneQuote:boolean=true;
+  showPro: boolean = false;
+  QuoteId: any;
+  LongQuote: any;
+  ShowOneQuote: boolean = true;
 
 
   url: string | ArrayBuffer | null | undefined;
-  Submitted=false
+  Submitted = false
   fileData = [] as any;
   fileImageName: any = '';
   isImage = true;
@@ -76,34 +76,34 @@ LongQuote:any;
   Linkedin: any;
   Twitter: any;
   faceB: any;
- 
+
   showMainContent: Boolean = true;
- templatData:boolean=true;
-// fontSizeChange:any=3
-// QR Code
-elementType = NgxQrcodeElementTypes.URL;
-correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
-value = ""
-// change new
-templateFontSize:any=24;
-public contactDetailColor: string = '#e920e9';
-public lastNameColor: string = '#fff500';
-public designationColor: string = 'rgb(236,64,64)';
-public firstNameColor: string = '#2d5964';
-fontFamilyNew:any = 'Poppins, sans-serif'
-lineHeight:any=1.5
+  templatData: boolean = true;
+  // fontSizeChange:any=3
+  // QR Code
+  elementType = NgxQrcodeElementTypes.URL;
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  value = ""
+  // change new
+  templateFontSize: any = 24;
+  public contactDetailColor: string = '#e920e9';
+  public lastNameColor: string = '#fff500';
+  public designationColor: string = 'rgb(236,64,64)';
+  public firstNameColor: string = '#2d5964';
+  fontFamilyNew: any = 'Poppins, sans-serif'
+  lineHeight: any = 1.5
   tempDetails: any;
-  templateDesign:any = {
-    firstNameColor:this.firstNameColor,
-    lastNameColor:this.lastNameColor,
-    designationColor:this.designationColor,
-    contactDetailColor:this.contactDetailColor,
-    fontFamily:this.fontFamilyNew,
-    fontSize:this.templateFontSize,
-    lineHeight:this.lineHeight,
-    
+  templateDesign: any = {
+    firstNameColor: this.firstNameColor,
+    lastNameColor: this.lastNameColor,
+    designationColor: this.designationColor,
+    contactDetailColor: this.contactDetailColor,
+    fontFamily: this.fontFamilyNew,
+    fontSize: this.templateFontSize,
+    lineHeight: this.lineHeight,
+
   }
-  fontFamilyList:any = [ 'Poppins, sans-serif','serif',
+  fontFamilyList: any = ['Poppins, sans-serif', 'serif',
     'sans-serif',
     'monospace',
     'cursive',
@@ -116,29 +116,32 @@ lineHeight:any=1.5
     'emoji',
     'math',
     'fangsong']
-// chnage end
-getScanText() {
-  let token:any = sessionStorage.getItem('ClientSpark')
-  this.value = 'https://app.giveaspark.com/home-dashboard/myprofile/profile-dashboard?token='+btoa(token)
-  // this.router.navigateByUrl('home-dashboard/myprofile/profile-dashboard?token='+btoa(token))
-}
+  compPhone: any;
+  // chnage end
+  getScanText() {
+    let token: any = sessionStorage.getItem('ClientSpark')
+    this.value = 'https://app.giveaspark.com/home-dashboard/myprofile/profile-dashboard?token=' + btoa(token)
+    // this.router.navigateByUrl('home-dashboard/myprofile/profile-dashboard?token='+btoa(token))
+  }
 
   constructor(private api: SparkService,
-    private fb: FormBuilder, private toast:ToastrService, private router:Router ) {
+    private fb: FormBuilder, private toast: ToastrService, private router: Router) {
     this.signatureDetailsForm = this.fb.group({
-      yourName:[''],
-      designation:[''],
-      email:[''],
-      phoneNo:[''],
-      companyWebsite:[''],
-      address:[''],
-      fbProfile:[''],
-      instagramProfile:[''],
-      linkedInProfile:[''],
-      youtubeChannel:[''],
-      quotesId:[''],
-      quotes:[''],
-      profileImage:['']
+      yourName: [''],
+      designation: [''],
+      email: [''],
+      phoneNo: [''],
+      companyWebsite: [''],
+      address: [''],
+      fbProfile: [''],
+      instagramProfile: [''],
+      linkedInProfile: [''],
+      youtubeChannel: [''],
+      quotesId: [''],
+      quotes: [''],
+      profileImage: [''],
+      companyPhone: [''],
+      twitterProfile: ['']
     })
   }
 
@@ -147,48 +150,48 @@ getScanText() {
     // this.getMyStories();
     // this.getmyQuote();
     this.getTemplateDetails();
-  if(localStorage.getItem('quoteId')){
-    this.QuoteId = localStorage.getItem('quoteId')
+    if (localStorage.getItem('quoteId')) {
+      this.QuoteId = localStorage.getItem('quoteId')
 
-   }
-   
-   if(localStorage.getItem('LongQuotes')){
-    this.LongQuote= localStorage.getItem('LongQuotes')
-  
-   }
-   console.log(this.LongQuote, this.QuoteId)
-  this.getBasicProfile();
-  this.getScanText();
+    }
+
+    if (localStorage.getItem('LongQuotes')) {
+      this.LongQuote = localStorage.getItem('LongQuotes')
+
+    }
+    console.log(this.LongQuote, this.QuoteId)
+    this.getBasicProfile();
+    this.getScanText();
 
 
 
   }
 
   // new change start
-  changeSize(evt:any) {
-    let currnetSize = Number(evt.target.value);        
-    if(currnetSize == 4) {
-      this.templateFontSize = 34            
-    } else if(currnetSize == 5) {
-      this.templateFontSize = 44      
-    } else if(currnetSize == 3) {
+  changeSize(evt: any) {
+    let currnetSize = Number(evt.target.value);
+    if (currnetSize == 4) {
+      this.templateFontSize = 34
+    } else if (currnetSize == 5) {
+      this.templateFontSize = 44
+    } else if (currnetSize == 3) {
       this.templateFontSize = 24
-    } else if(currnetSize == 2) {
+    } else if (currnetSize == 2) {
       this.templateFontSize = 20
     } else {
       this.templateFontSize = 10
     }
-  
+
   }
-  changeLine(evt:any){
-    let currnetSize = Number(evt.target.value);        
-    if(currnetSize == 4) {
-      this.lineHeight = 2.4            
-    } else if(currnetSize == 5) {
-      this.lineHeight = 3.4      
-    } else if(currnetSize == 3) {
+  changeLine(evt: any) {
+    let currnetSize = Number(evt.target.value);
+    if (currnetSize == 4) {
+      this.lineHeight = 2.4
+    } else if (currnetSize == 5) {
+      this.lineHeight = 3.4
+    } else if (currnetSize == 3) {
       this.lineHeight = 1.4
-    } else if(currnetSize == 2) {
+    } else if (currnetSize == 2) {
       this.lineHeight = 1.1
     } else {
       this.lineHeight = 0.5
@@ -199,23 +202,23 @@ getScanText() {
   getBasicProfile() {
     this.api.getbasicDetaiofUseer().subscribe((res: any) => {
       this.details = res.result[0];
-      console.log(res,'xasds');
-      
+      console.log(res, 'xasds');
+
       this.username = res.result[0]?.firstName
-    this.Email = res.result[0]?.email
-    this.compName = res.result[0]?.companyName
-    this.compWebsite = res.result[0]?.companyWebsite
-    this.number = res.result[0]?.phone
+      this.Email = res.result[0]?.email
+      this.compName = res.result[0]?.companyName
+      this.compWebsite = res.result[0]?.companyWebsite
+      this.number = res.result[0]?.phone
+      this.compPhone = res.result[0]?.companyPhone
+      this.desig = res.result[0]?.designation
+      this.addr = res.result[0]?.address
 
-    this.desig= res.result[0]?.designation
-    this.addr=res.result[0]?.addressne
+      this.insta = res.result[0]?.instagramProfile
+      this.uTube = res.result[0]?.youtubeChannel
 
-    this.insta = res.result[0]?.instagramProfile
-    this.uTube = res.result[0]?.youtubeChannel
-
-    this.Linkedin= res.result[0]?.linkedInProfile
-    this.Twitter=res.result[0]?.addressne
-      this.faceB=res.result[0]?.fbProfile
+      this.Linkedin = res.result[0]?.linkedInProfile
+      this.Twitter = res.result[0]?.twitterProfile
+      this.faceB = res.result[0]?.fbProfile
 
 
       console.log(this.compWebsite, 'jojo')
@@ -226,44 +229,48 @@ getScanText() {
 
 
   }
- getTemplateDetails(){
-  this.api.getsignatureDetails().subscribe((res:any)=>{
+  getTemplateDetails() {
+    this.api.getsignatureDetails().subscribe((res: any) => {
 
-    this.tempDetails = res.result[res.result.length -1];
-console.log(this.tempDetails, "kkkkkkkk")
- 
- 
-  })
- }
+      this.tempDetails = res.result[res.result.length - 1];
+      console.log(this.tempDetails, "kkkkkkkk")
 
 
-  onSubmit(data:any) {
+    })
+  }
+
+
+  onSubmit(data: any) {
 
     let body = {
 
-      yourName:data.yourName,
-      designation:data.designation,
-      email:data.email,
-      phoneNo:data.phoneNo,
-      companyWebsite:data.companyWebsite,
+      yourName: data.yourName,
+      designation: data.designation,
+      email: data.email,
+      phoneNo: data.phoneNo,
+      companyWebsite: data.companyWebsite,
       address: [
-        
-           { addressline: data.address,
-                      addressline2: data.address,
-            landmark:data.address,
-            city: data.address,
-            state: data.address,
-            pincode: '',
-            country: data.address,}
-  
-    ],
-      fbProfile:data.fbProfile,
-      instagramProfile:data.instagramProfile,
-      linkedInProfile:data.linkedInProfile,
-      youtubeChannel:data.youtubeChannel,   
-      quotesId:this.QuoteId,
-      quotes:data.quotes,
-      profileImage:this.imageData2
+
+        {
+          addressline: data.address,
+          addressline2: data.address,
+          landmark: data.address,
+          city: data.address,
+          state: data.address,
+          pincode: '',
+          country: data.address,
+        }
+
+      ],
+      fbProfile: data.fbProfile,
+      twitterProfile: data?.twitterProfile,
+      instagramProfile: data.instagramProfile,
+      linkedInProfile: data.linkedInProfile,
+      youtubeChannel: data.youtubeChannel,
+      quotesId: this.QuoteId,
+      quotes: data.quotes,
+      profileImage: this.imageData2,
+      companyPhone: data.companyPhone
 
     }
 
@@ -284,7 +291,7 @@ console.log(this.tempDetails, "kkkkkkkk")
   showOnPlan() {
     this.planShow == true
   }
-  addQuote(){
+  addQuote() {
 
     this.router.navigate(['/home-dashboard/motivational-quote/quote-dashboard'])
   }
@@ -301,21 +308,19 @@ console.log(this.tempDetails, "kkkkkkkk")
   //   })
   // }
 
- 
-  openTemp(){
-   if( this.showPro =!this.showPro){
-    this.templatData=false;
-   }
-  
+
+  openTemp() {
+    if (this.showPro = !this.showPro) {
+      this.templatData = false;
+    }
+
   }
 
 
 
-onClick(){
+  onClick() {
 
-}
-
-
+  }
 
 
 
@@ -329,49 +334,51 @@ onClick(){
 
 
 
-onSelectImage(event: any) {
-  this.Submitted=true;
-  let files = event.target.files;
-  this.fileImageName = event.target.files[0].name;
-  if (files) {
-    this.uploadImage = files[0]
-    this.subImageSubmit()
-    for (let file of files) {
-      if (!file.type.includes('image')) {
-        this.isImage = false;
-        return;
+
+
+  onSelectImage(event: any) {
+    this.Submitted = true;
+    let files = event.target.files;
+    this.fileImageName = event.target.files[0].name;
+    if (files) {
+      this.uploadImage = files[0]
+      this.subImageSubmit()
+      for (let file of files) {
+        if (!file.type.includes('image')) {
+          this.isImage = false;
+          return;
+        }
+        this.fileData.push(file);
       }
-      this.fileData.push(file);
+    }
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event) => {
+        this.url = event.target?.result;
+      };
     }
   }
-  if (event.target.files && event.target.files[0]) {
-    var reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    reader.onload = (event) => {
-      this.url = event.target?.result;
-    };
-  }
-}
-subImageSubmit(){
-  this.Submitted=true
-  let formData = new FormData();    
-  formData.append('attachment', this.uploadImage);
-  this.api.addAttachments(formData).subscribe(
-    (res: any) => {
-      console.log(res);
-      this.imageData1 = res;
-      this.imageData2 = this.imageData1[0].key;
-      console.log(this.imageData1[0].key, "image key ")
-      
-    },
-    (err: any) => {
-  
-      console.log(err);
-      
-    }
-  )
+  subImageSubmit() {
+    this.Submitted = true
+    let formData = new FormData();
+    formData.append('attachment', this.uploadImage);
+    this.api.addAttachments(formData).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.imageData1 = res;
+        this.imageData2 = this.imageData1[0].key;
+        console.log(this.imageData1[0].key, "image key ")
 
-}
+      },
+      (err: any) => {
+
+        console.log(err);
+
+      }
+    )
+
+  }
 
 
 
